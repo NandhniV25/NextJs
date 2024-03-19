e035e4df651e31919c713752b46e6f317ef8d407
Nandhni G A <nandhni.25@gmail.com>
2024-03-19 16:16:51 +0530

    edge case of create invoice error removed

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -30,7 +30,7 @@ export type State = {
     message?: string | null;
   };
 
-  export async function createInvoice(prevState: State, formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
     // Validate form using Zod
     const validatedFields = CreateInvoice.safeParse({
       customerId: formData.get('customerId'),
@@ -68,24 +68,37 @@ export type State = {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+
+  export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+  ) {
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
    
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
+    }
+   
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
    
     try {
-        await sql`
-            UPDATE invoices
-            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-            WHERE id = ${id}
-          `;
-      } catch (error) {
-        return { message: 'Database Error: Failed to Update Invoice.' };
-      }
+      await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
