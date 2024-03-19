37ec9d6ebbb464b27dfdd7bacd33810c0b13c304
Nandhni G A <nandhni.25@gmail.com>
2024-03-19 14:47:05 +0530

    modify actions.ts deleteInvoice

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -68,7 +68,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 
   export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    //throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
@@ -76,5 +76,5 @@ export async function updateInvoice(id: string, formData: FormData) {
       } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice.' };
       }
-    revalidatePath('/dashboard/invoices');
+    //revalidatePath('/dashboard/invoices');
   }
